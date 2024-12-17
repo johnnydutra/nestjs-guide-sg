dfb42986815b3a9fc409e9cd52acbe6c49cf1c22
Johnny Dutra <johnny.dutra@gmail.com>
2024-12-17 16:13:14 -0300

    64: how to build interceptors

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,23 @@
+import {
+  UseInterceptors,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { plainToClass } from 'class-transformer';
+
+export class SerializeInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    console.log('Running before the handler', context);
+    return next.handle().pipe(
+      map((data: any) => {
+        console.log('Running before response is sent out', data);
+      }),
+    );
+  }
+}
