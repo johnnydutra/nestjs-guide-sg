b27e6a9f518fe8f768ba6d5ad59a71ad52c42136
Johnny Dutra <johnny.dutra@gmail.com>
2024-12-18 11:10:30 -0300

    79: signing in an user

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const cookieSession = require('cookie-session');
 
 async function bootstrap() {
